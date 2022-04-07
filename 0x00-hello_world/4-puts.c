f796d5b18b299aee450ec9ded5e8c7324c75fee8
hamisiswalehe <hamisiswaleh1@gmail.com>
2022-04-07 14:32:30 -0700

    we

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,7 @@
-#!/bin/bash
-#include <stdio.h>                                                                                                                     
-
-int main(void)                                                                                                                         
-
-{                                                                                                                                      
-
-puts("\"Programming is like building a multilingual puzzle");                                                                          
-
-                                                                                                                                       
-
-return (0);                                                                                                                            
-
-                                                                                                                                       
-
+#include <stdio.h>
+int main(void)
+{
+puts(""Programming is like building a multilingual puzzle");
+puts();
+return (0);
 }
