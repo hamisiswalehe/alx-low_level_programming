902e1122999eb94880b99fae272ec8734d536460
hamisiswalehe <hamisiswaleh1@gmail.com>
2022-04-08 16:02:26 -0700

    we

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
-#include <stdlib.h>                                                                                                                 
-#include <time.h>                                                                                                                  
-#include <stdio.h>                                                                                                                  
-/**                                                                                                                                 
- * main - main block                                                                                                              
- * Description:lowercase
- * Return: 0                                                                                                                       
-*/                                                                                                                                  
-int main(void)                                                                                                                      {
-	char c = 'a';
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block
+* Description: Get a random number
+* Return: 0
+*/
 
-	while (c <= 'z')
-	{
-              putchar(c);
-	      c++;
-	}
-        putchar('\n');
-        return (0);
-}	                                                                                                        
+int main(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+return (0);
+}
